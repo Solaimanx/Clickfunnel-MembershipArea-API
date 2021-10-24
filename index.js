5e8e315cd4a19507177b38f2867a0e5696d00d93
Mohammad Solaiman <solaiman321@gmail.com>
2021-10-25 00:39:14 +0600

    added origin

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ const sgMail = require("@sendgrid/mail");
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 const corsOptions = {
-  origin: "*",
+  origin: "english21days.co.il",
   methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
   optionsSuccessStatus: 200,
   credentials: true,
