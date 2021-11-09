0283138b99936f7a7b6ac034b81b0ef8a39be9a9
Mohammad Solaiman <solaiman321@gmail.com>
2021-11-10 02:50:39 +0600

    added forgot password email with schedule system

--- index.js
+++ index.js
@@ -265,6 +265,154 @@ ${password}
 });
 
 
+//send email 2nd time after waiting 15 mins
+app.get("/forgot-password/:email", async (req, res) => {
+
+
+  res.setHeader("Access-Control-Allow-Origin", "https://www.english21days.co.il");
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,Content-Type, Authorization,Accept"
+  );
+
+  const { email } = req.params;  
+
+
+  const currectTime = new Date().getTime() + 15 * 60 * 1000;
+  const waiting = new Date(currectTime);
+
+  schedule.scheduleJob(waiting, async function () {
+
+
+
+  const msg = {
+    to: email,
+    from:  ' אריאל אפל (קונפידנס לימוד אנגלית) <info@english21days.co.il>',
+    subject: `פרטי הגישה שלך `,
+
+    html: `
+    <table style="  direction:rtl ; text-align:right ; width:100%;border-spacing:0px;border-collapse:collapse;border-width:medium;border-style:none" role="presentation">
+                                  <tbody>
+                                    <tr>
+                                      <td style="padding:0px;width:100%" width="100%" valign="top">
+                                        <div>
+                                          <div style="font-size:16px">
+                                            <div>
+                                              <div>הי!</div>
+                                              <div><br>
+                                                לאחרונה ביקשת לשנות
+                                                סיסמא לחשבונך.<br>
+                                                <br>
+                                                במידה וכבר שינית את
+                                                הסיסמא, <br>
+                                                פשוט תיכנס מכאן עם המייל
+                                                והסיסמא שיצרת:<br>
+                                                <a href="https://www.english21days.co.il/course33523349" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.english21days.co.il/course33523349&amp;source=gmail&amp;ust=1636575999458000&amp;usg=AFQjCNEF7xl87vTFe_um9yW-jR9TvcS_Vg">https://www.english21days.co.<wbr>il/course33523349</a><br>
+                                                (כדאי לשמור את הקישור
+                                                הזה במועדפים שיהיה לך קל
+                                                למצוא)<br>
+                                                <br>
+                                                במידה ועדיין לא שינית את
+                                                הסיסמא,<br>
+                                                ניתן לעשות זאת <a href="https://www.english21days.co.il/thank-you-basic-plankz1yvwq9" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.english21days.co.il/thank-you-basic-plankz1yvwq9&amp;source=gmail&amp;ust=1636575999458000&amp;usg=AFQjCNF3Ig2WYhHdQCSwVc63BWr3xE7OeA">מכאן</a>.<br>
+                                              </div>
+                                              <div><br>
+                                              </div>
+                                              <div><br>
+                                                מאחל לך לימוד נעים
+                                                ומהנה,<br>
+                                                אריאל</div>
+                                              <div>&nbsp;</div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div><span>
+                                            <table style="float:none;text-align:right;border-spacing:0px;border-collapse:collapse;border-width:medium;border-style:none" role="presentation" width="100%" align="right">
+                                              <tbody>
+                                                <tr>
+                                                  <td style="padding:0px 0px 0px 0px" align="right"> <img src="https://ci6.googleusercontent.com/proxy/NFq5F8FXTyJM6yeepc3Yca4Wo9kAasjJH5cknq06Iay5PluCdAi795i2M8R2oazXeMZqekoKLOcOkORHaj7eedPPJUVIvc4AolyJdgerYxFbZkcZwd6sCUmMn_MQ_n_r8mWa52APVI_YWSoXCWDyArOoQijTwf59yQ=s0-d-e1-ft#https://hostedimages-cdn.aweber-static.com/MTAyNTkyMA==/thumbnail/697ac1bb04e54e1f87d58b5e3a11c7b4.png" style="display:block;width:136px;height:150px;max-width:100%" alt="" width="136" height="150" class="CToWUd"> </td>
+                                                </tr>
+                                              </tbody>
+                                            </table>
+                                          </span></div>
+                                        <div>
+                                          <div style="font-size:16px">
+                                            <div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+    
+    `,
+  };
+  await sgMail.send(msg, function (err, info) {
+    if (err) {
+      console.log(`Email Not Sent Error Occured => ${err}`);
+    } else {
+      console.log(`Email was Sent`);
+    }
+  });
+
+
+
+
+
+
+
+
+    
+  });
+
+  return res.status(200).json({ message: "success" });
+
+
+
+
+
+
+});
+
+
+
+
+
+
 app.get('/password',  (req, res) => {
 
   res.setHeader("Access-Control-Allow-Origin", "https://www.english21days.co.il");
