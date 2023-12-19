eb079ea2303702170c1b2eaf58c452388eae326a
mohammad solaiman <solaiman321@gmail.com>
2023-12-20 01:03:36 +0600

    fixed json res

--- chatGPT.js
+++ chatGPT.js
@@ -17,7 +17,7 @@ const getQuestionsBasedOnTopic = async (req, res) => {
         },
         {
           role: "user",
-          content: `List 5 common sentences in plain English and translate it to hebrew (complete sentences, no brackets, ${level}) on the following topic: ${topic} ) put into differnt array , english array name is englishQuestions , hebrew array name is hebrewQuestions`,
+          content: `List 25 common sentences in plain English and translate it to hebrew (complete sentences, no brackets, ${level}) on the following topic: ${topic} ) put into differnt array , english array name is englishQuestions , hebrew array name is hebrewQuestions`,
         },
       ],
       model: "gpt-3.5-turbo-1106",
@@ -30,7 +30,7 @@ const getQuestionsBasedOnTopic = async (req, res) => {
       .replaceAll("", "")
       .trim();
     const data = JSON.parse(parsed);
-    return res.json({ ...data });
+    return res.json({ data });
   } catch (err) {
     console.log(err);
     res.status(404).send("failed");
